7d6b57e3edba8a8aa31fbeb1f0a3b4eed029932e
padi-dev-deantr <tungnd1@paditech.com>
2025-10-15 16:09:57 +0700

    feat : handle entrypoint 20s

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ echo "⏳ Waiting for MySQL to be ready at ${DB_HOST}:${DB_PORT}..."
 # Find mysql server
 until nc -z -v -w30 $DB_HOST $DB_PORT; do
   echo "❗ MySQL is unavailable - sleeping 3s"
-  sleep 3
+  sleep 20
 done
 
 echo "✅ MySQL is up! Starting the application..."
